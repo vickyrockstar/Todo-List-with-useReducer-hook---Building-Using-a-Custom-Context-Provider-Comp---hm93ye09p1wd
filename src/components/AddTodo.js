dc979ed85a2860259bb80b0d9a8cb940304a160d
vighnesh shetty <116479689+vickyrockstar@users.noreply.github.com>
2023-04-03 20:27:08 +0530

    updateall

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,24 @@
-import React from "react"
+import React,{useRef} from "react"
 
 const AddTodo = ({ dispatch }) => {
-
+    const inputReference = useRef();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let title = inputReference.current.value;
+        if(!title) return;
+        let id = Date.now();
+        dispatch({type : 'addTodo', obj :{title,id}})
+        inputReference.current.value = '';
+    }
 
     return (
-        <></>
+        <>
+        <form id="todo-form" onSubmit={handleSubmit}>
+            <label htmlFor="todo-input">Title</label>
+            <input type="text" id="todo-input" ref={inputReference}/>
+            <button type="submit">Add To-Do</button>
+        </form>
+        </>
     )
 }
 
